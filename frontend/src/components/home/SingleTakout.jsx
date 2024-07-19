28d1dffaa09091143648e4e4ddbd006669b87946
edidiesky <essienedidiong1000@gmail.com>
2024-07-19 06:42:53 -0700

    new fixes

--- frontend/src/components/home/SingleTakout.jsx
+++ frontend/src/components/home/SingleTakout.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function SingleTakout() {
+  const { menus } = useSelector((store) => store.menu);
   return (
     <div className="w-full min-h-[100vh] flex items-center justify-center py-40 relative">
       <img
@@ -12,19 +14,18 @@ export default function SingleTakout() {
       <div className="flex md:flex-row flex-col w-85 z-40 auto">
         <div className="flex-1 md:flex-[0.5] bg-[#000] flex-col flex gap-12 px-12 md:px-20 py-24">
           <h1 className="family3 flex items-start gap-4 justify-between leading-[1.6] text-6xl md:text-7xl text-white">
-            <span className="leading-[1.3]">BRAISED ABALONE</span>
-            <span className="family1 pt-8 text-xl">$30</span>
+            <span className="leading-[1.3]">{menus[0]?.title}</span>
+            <span className="family1 pt-8 text-xl">${menus[0]?.price}</span>
           </h1>
           <h4 className="text-xl md:text-2xl  family2 text-light text-[var(--grey-1)]">
-            Pretium accumsan porttitor viverra leo gravida mollis imperdiet.
-            Fringilla nibh pharetra sociis leo amet.
+            {menus[0]?.description}
           </h4>
           <div className="w-full text-start">
             <Link
-              to={`restaurant/takeout/${"BRAISED ABALONE"}`}
+              to={`restaurant/takeout/${menus[0]?.title}`}
               className="btn text-dark btn-2 family1 uppercase text-white text-light text-base py-2"
             >
-              View takeout Menu
+              View This Menu
             </Link>
           </div>
         </div>
--- frontend/src/components/home/TakeoutList.jsx
+++ frontend/src/components/home/TakeoutList.jsx
@@ -2,69 +2,145 @@ import React from "react";
 import styled from "styled-components";
 import { planData } from "../../data/Plan";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 export default function Plans() {
+  const { menus } = useSelector((store) => store.menu);
+  // Hors d’oeuvres
+  // Main Course
+  // Gin & Tonic
+
+  const maincourse = menus.filter((data) => data.category === "Main Course");
+  const houres = menus.filter((data) => data.category === "Hors d’oeuvres");
+  // get desserts collections
+  const desserts = menus.filter((data) => data.category === "desserts");
+  const DRINK = menus.filter((data) => data.category === "DRINK & COCKTAIL");
   return (
     <PlansContent>
       <div className="w-85 px-16 auto container">
         <div className="w-full gap-8 md:gap-0 grid md:grid-cols-3">
-          {planData.map((x, index) => {
-            return (
-              <div
-                className={
-                  index === 1
-                    ? "Card w-full flex-col gap-3 items-center flex bg-[#fff] active"
-                    : "Card w-full flex-col gap-3 items-center flex bg-[#fff]"
-                }
-                key={index}
+          <div className="w-full Card flex-col gap-3">
+            <div className="w-85 auto px-8 flex flex-col gap-12">
+              <h2
+                className={`text-[#000] family3 text-5xl uppercase text-start`}
               >
-                <div className="w-[70%] mx-auto flex flex-col gap-12 justify-center items-start">
-                  <h2
-                    className={`${
-                      index === 1 ? "text-[#fff]" : "text-[#000"
-                    } family3 text-5xl uppercase text-start`}
-                  >
-                    {x.title}
-                  </h2>
-
-                  <ul className="w-full flex flex-col gap-6 py-3">
-                    {x.list.map((x) => {
-                      return (
-                        // eslint-disable-next-line react/jsx-key
-                        <Link to={`/restaurant/takeout/${x?.title}`} className="w-full" key={index}>
-                          <li className="w-full auto flex flex-col gap-2 text-base center border-bottom py-1 auto">
-                            <div className="flex w-full items-center gap-2 justify-space">
-                              <div className="flex w-full justify-between gap-4 items-center">
-                                <h3
-                                  className={`${
-                                    index === 1 ? "text-[#fff]" : "text-[#000"
-                                  } family3 text-4xl text-dark`}
-                                >
-                                  {x.title}
-                                </h3>
-                                <span className={`${
-                                index === 1 ? "text-[var(--grey-1)]" : "text-[#000"
-                              } text-xl text-dark`}>
-                                  ${x.price}
-                                </span>
-                              </div>
+                Hors d’oeuvres
+              </h2>
+              <div className="w-full flex flex-col gap-4">
+                {houres?.map((x, index) => {
+                  return (
+                    <div key={index} className=" w-full items-center flex">
+                      <Link
+                        to={`/restaurant/takeout/${x?.title}`}
+                        className="w-full"
+                        key={index}
+                      >
+                        <li className="w-full auto flex flex-col gap-2 text-base center border-bottom py-1 auto">
+                          <div className="flex w-full items-center gap-2 justify-space">
+                            <div className="flex w-full justify-between gap-4 items-center">
+                              <h3 className={`family3 text-4xl text-dark`}>
+                                {x?.title}
+                              </h3>
+                              <span className={`text-xl text-dark`}>
+                                ${x?.price}
+                              </span>
                             </div>
-                            <h4
-                              className={`${
-                                index === 1 ? "text-[var(--grey-1)]" : "text-[#000"
-                              } text-dark family2 text-xl`}
-                              // className="family2 text-xl"
-                            >
-                              {x.desc}
-                            </h4>
-                          </li>
-                        </Link>
-                      );
-                    })}
-                  </ul>
-                </div>
+                          </div>
+                          <h4
+                            className={`text-dark family2 text-xl`}
+                            // className="family2 text-xl"
+                          >
+                            {x?.description}
+                          </h4>
+                        </li>
+                      </Link>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
+            </div>
+          </div>
+          <div className="w-full Card active flex-col gap-3">
+            <div className="w-85 auto px-8 flex flex-col gap-12">
+              <h2
+                className={`text-[#fff] family3 text-5xl uppercase text-start`}
+              >
+                Main Course
+              </h2>
+              <div className="w-full flex flex-col gap-4">
+                {maincourse?.map((x, index) => {
+                  return (
+                    <div key={index} className=" w-full items-center flex">
+                      <Link
+                        to={`/restaurant/takeout/${x?.title}`}
+                        className="w-full"
+                        key={index}
+                      >
+                        <li className="w-full auto flex flex-col gap-2 text-base center border-bottom py-1 auto">
+                          <div className="flex w-full items-center gap-2 justify-space">
+                            <div className="flex w-full justify-between gap-4 items-center">
+                              <h3 className={`family3 text-4xl text-[#fff]`}>
+                                {x?.title}
+                              </h3>
+                              <span className={`text-xl text-[var(--grey-1)]`}>
+                                ${x?.price}
+                              </span>
+                            </div>
+                          </div>
+                          <h4
+                            className={`text-[var(--grey-1)] family2 text-xl`}
+                            // className="family2 text-xl"
+                          >
+                            {x?.description}
+                          </h4>
+                        </li>
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="w-full Card flex-col gap-3">
+            <div className="w-85 auto px-8 flex flex-col gap-12">
+              <h2
+                className={`text-[#000] family3 text-5xl uppercase text-start`}
+              >
+                Desserts
+              </h2>
+              <div className="w-full flex flex-col gap-4">
+                {desserts?.map((x, index) => {
+                  return (
+                    <div key={index} className=" w-full items-center flex">
+                      <Link
+                        to={`/restaurant/takeout/${x?.title}`}
+                        className="w-full"
+                        key={index}
+                      >
+                        <li className="w-full auto flex flex-col gap-2 text-base center border-bottom py-1 auto">
+                          <div className="flex w-full items-center gap-2 justify-space">
+                            <div className="flex w-full justify-between gap-4 items-center">
+                              <h3 className={`family3 text-4xl text-dark`}>
+                                {x?.title}
+                              </h3>
+                              <span className={`text-xl text-dark`}>
+                                ${x?.price}
+                              </span>
+                            </div>
+                          </div>
+                          <h4
+                            className={`text-dark family2 text-xl`}
+                            // className="family2 text-xl"
+                          >
+                            {x?.description}
+                          </h4>
+                        </li>
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </PlansContent>
