2465aadefdf14b813d8703bc519559168f905fec
edidiesky <essienedidiong1000@gmail.com>
2024-07-27 02:39:39 -0700

    new fixes

--- frontend/src/components/home/TakeoutList.jsx
+++ frontend/src/components/home/TakeoutList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { planData } from "../../data/Plan";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 export default function Plans() {
