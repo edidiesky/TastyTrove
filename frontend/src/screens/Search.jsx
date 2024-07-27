ba99e36bb28c2772e805067c903e568d21551f02
edidiesky <essienedidiong1000@gmail.com>
2024-07-27 02:28:07 -0700

    new fixes

--- frontend/src/screens/Search.jsx
+++ frontend/src/screens/Search.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import HomeIndex from "../components/search";
 import Meta from "@/components/common/Meta";
-import Navbar from "@/components/common/Navbar";
+// import Navbar from "@/components/common/Navbar";
 const Search = () => {
   return (
     <div>
       <Meta title={"Search for quality homes"} />
-      <Navbar />
+      {/* <Navbar /> */}
       <HomeIndex />
     </div>
   );
