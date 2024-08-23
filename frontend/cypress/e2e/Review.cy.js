d61704004879ab594aa68dc5c91695baa3d51e6e
edidiesky <essienedidiong1000@gmail.com>
2024-08-23 05:16:23 -0700

    new fixes

--- frontend/cypress/e2e/Review.cy.js
+++ frontend/cypress/e2e/Review.cy.js
@@ -15,18 +15,25 @@ describe("Tetsing the menu review form fields", () => {
     cy.getDataTest("review_button").should("be.disabled");
   });
   // fill the input fields and select the review tab and check that the button is not disabled
-  it("Should fill the input fields and select the review tab and the button should not be diabled", () => {
+  it.only("Should fill the input fields and select the review tab and the button should not be diabled", () => {
     cy.wait(4000);
     cy.getDataTest("review_tab_4").click();
     cy.getDataTest("description").type("A nice meal");
     cy.getDataTest("review_button").should("not.be.disabled");
   });
 
-  // fill an empty input fields and select the review tab and check that the button should be disabled
-  it.only("Should fill the input fields and select the review tab and the button should not be diabled", () => {
+  // select only the review tab and check that the button should be disabled
+  it("Should select only the review tab and check that the button should be disabled", () => {
     cy.wait(4000);
     cy.getDataTest("review_tab_4").click();
-    cy.getDataTest("review_button").should("not.be.disabled");
+    cy.getDataTest("review_button").should("be.disabled");
+  });
+  // fill only the input fields and dont select the review tab and check that the button is disabled
+
+  it("should fill only the input fields and don't select the review tab and also check that the button is disabled", () => {
+    cy.wait(4000);
+    cy.getDataTest("description").type("A nice meal");
+    cy.getDataTest("review_button").should("be.disabled");
   });
   // submit the form
   // check that the review length is more than 1
