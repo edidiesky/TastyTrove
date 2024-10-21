a90223a071ae27241c8d5ba044792fcc7c41bbf5
edidiesky <essienedidiong1000@gmail.com>
2024-10-21 13:25:35 -0700

    Fixed the loader spinner

--- frontend/src/components/loader/index.jsx
+++ frontend/src/components/loader/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ThreeDots } from "react-loader-spinner";
-const Loader = ({ type, color }) => {
+import { CiSettings } from "react-icons/ci";
+const Loader = ({ type, color, size }) => {
   if (type === "dots") {
     return (
       <ThreeDots
-        height="24"
-        width="24"
+        height={size ? size : "20"}
+        width={size ? size : "20"}
         radius="10"
-        color={color ? color : "#000"}
+        color={color ? color : "#fff"}
         ariaLabel="three-dots-loading"
         wrapperStyle={{}}
         wrapperClassName=""
@@ -17,24 +18,27 @@ const Loader = ({ type, color }) => {
   }
   return (
     <div
-      className="flex items-center top-0 left-0 z-[10000000] justify-center"
+      className="flex items-center top-0 left-0 z-[333330000000] justify-center"
       style={{
         width: "100vw",
         position: "fixed",
         height: "100vh",
-        background: "#ffffffc3",
+        background: "#ffffff7a",
       }}
     >
-      <ThreeDots
-        height="70"
-        width="70"
+      <div className="spinner text-xl">
+        <CiSettings fontSize={'30px'} />
+      </div>
+      {/* <ThreeDots
+        height="40"
+        width="40"
         radius="10"
         color={"#000"}
         ariaLabel="three-dots-loading"
         wrapperStyle={{}}
         wrapperClassName=""
         visible={true}
-      />
+      /> */}
     </div>
   );
 };
