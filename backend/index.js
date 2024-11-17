b4e2e922cd2d6fbbebce16b1d66bb5e012581c7d
edidiesky <essienedidiong1000@gmail.com>
2024-11-16 23:32:30 -0800

    Message fixes

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,8 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.WEB_ORIGIN,
+    methods: ["GET", "POST"],
+    credentials: true, // Allow cookies
   },
 });
 
